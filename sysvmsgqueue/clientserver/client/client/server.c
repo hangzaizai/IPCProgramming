9dafa9fd8e3f732f10da1c1066faf45cd28c2fed
249027600@qq.com <249027600@qq.com>
2017-10-20 17:01:09 +0800

    添加工程文件

--- sysvmsgqueue/clientserver/client/client/server.c
+++ sysvmsgqueue/clientserver/client/client/server.c
@@ -7,3 +7,23 @@
 //
 
 #include "server.h"
+#include "msg.h"
+#include "unixIPC.h"
+
+void server(int readfd,int writefd)
+{
+    FILE    *fp;
+    ssize_t n;
+    struct custom_msg msg;
+    
+    msg.msg_type = 1;
+    if ( (n = Msg_recv(readfd, &msg)) == 0 ) {
+        err_quit("pathname missing");
+    }
+    msg.msg_data[n] = '\0';
+    
+    if ( (fp = fopen(<#const char *restrict __filename#>, <#const char *restrict __mode#>)) ) {
+        <#statements#>
+    }
+    
+}
